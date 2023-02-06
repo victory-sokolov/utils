bbbdf7779cdccaea0f2f67c18d72df97ead44f44
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-02-06 21:27:50 +0200

    HTML escape and unescape function

--- src/html.ts
+++ src/html.ts
@@ -1,6 +1,44 @@
+import { flip } from './object';
+
+const htmlEscapes = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+};
+const htmlUnescapes = flip(htmlEscapes) as Record<string, string>;
+
+/** Used to match HTML entities and HTML characters. */
+const reUnescapedHtml = /[&<>"']/g;
+const reHasUnescapedHtml = RegExp(reUnescapedHtml.source);
+
+const reEscapedHtml = /&(?:amp|lt|gt|quot|#(0+)?39);/g;
+const reHasEscapedHtml = RegExp(reEscapedHtml.source);
+
 /**
  * Remove HTML tags from the text
  * @param text Text with HTML tags
  * @returns Text with HTML tags removed
  */
 export const removeHtmlTags = (text: string): string => text.replace(/<(?:.|\\n)*?>/gm, '');
+
+/**
+ * Escape HTML tags to entities
+ * @param str HTML string
+ * @returns Escaped HTML tags
+ */
+export const escape = (str: string) => {
+    return str && reHasUnescapedHtml.test(str) ? str.replace(reUnescapedHtml, (chr) => htmlEscapes[chr]) : str || '';
+};
+
+/**
+ * Unescape HTML entities
+ * @param str HTML string
+ * @returns Unescaped HTML entity
+ */
+export const unescape = (str: string) => {
+    return str && reHasEscapedHtml.test(str)
+        ? str.replace(reEscapedHtml, (entity) => htmlUnescapes[entity] || "'")
+        : str || '';
+};
--- tests/html.test.ts
+++ tests/html.test.ts
@@ -1,17 +1,27 @@
-import { removeHtmlTags } from '../src/html';
+import { escape, unescape, removeHtmlTags } from '../src/html';
 
 describe('removeHtmlTags', () => {
     test('remove tags', () => {
         expect(removeHtmlTags('<p>Hello, world!</p>')).toBe('Hello, world!');
-        expect(removeHtmlTags('<span>Hello, world!</span>')).toBe(
-            'Hello, world!'
-        );
-        expect(removeHtmlTags('<div>Hello, world!</div>')).toBe(
-            'Hello, world!'
-        );
+        expect(removeHtmlTags('<span>Hello, world!</span>')).toBe('Hello, world!');
+        expect(removeHtmlTags('<div>Hello, world!</div>')).toBe('Hello, world!');
     });
 
     test('no tags', () => {
         expect(removeHtmlTags('Hello, world!')).toBe('Hello, world!');
+    });
+});
+
+describe('escape HTML entities', () => {
+    test('escape html string', () => {
+        const html = '<html></html>';
+        expect(escape(html)).toBe('&lt;html&gt;&lt;/html&gt;');
+    });
+});
+
+describe('unescape HTML entities', () => {
+    test('unescape html string', () => {
+        const html = '<html></html>';
+        expect(unescape('&lt;html&gt;&lt;/html&gt;')).toBe(html);
     });
 });
