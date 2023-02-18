3ccb02b9ae1510112be3504d6557bd259ec3946b
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-02-18 14:51:45 +0200

    build: add semantic release

---
+++ release.config.js
@@ -0,0 +1,15 @@
+export const config = {
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/git',
+            {
+                assets: ['dist/*.js', 'dist/*.ts', 'dist/*.map'],
+                message: 'chore(release): ${nextRelease.version} [skip-ci]\n\n${nextRelease.notes}',
+            },
+        ],
+        '@semantic-release/github',
+    ],
+};
