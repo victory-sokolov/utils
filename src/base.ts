0a0318122fbb22de962bd72ff13308a6413991ea
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-04-28 11:12:12 +0300

    add bytesToSize function

--- src/base.ts
+++ src/base.ts
@@ -1,6 +1,22 @@
+const SIZE_UNITS = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
 /**
  * Pause execution
  * @param ms Pause in milliseconds
  * @returns
  */
 export const wait = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * Convert bytes to size in Human readable format
+ * @param bytes Bytes
+ * @returns Human readable size from bytes
+ */
+export const bytesToSize = (bytes: number): string => {
+    if (bytes === 0) return '0';
+    const exp = Math.floor(Math.log(bytes) / Math.log(1000));
+    const size = bytes / 1000 ** exp;
+    const short = Math.round(size);
+    const unit = exp === 0 ? '' : ' ' + SIZE_UNITS[exp - 1];
+    return short.toString() + unit;
+};
---
+++ tests/base.test.ts
@@ -0,0 +1,35 @@
+import { bytesToSize } from '../src/base';
+
+describe('bytesToSize', () => {
+    test('it should return "0" when given 0 bytes', () => {
+        expect(bytesToSize(0)).toBe('0');
+    });
+
+    test('it should correctly convert bytes to kilobytes', () => {
+        expect(bytesToSize(1024)).toBe('1 KB');
+        expect(bytesToSize(2048)).toBe('2 KB');
+        expect(bytesToSize(3072)).toBe('3 KB');
+        expect(bytesToSize(1536)).toBe('2 KB');
+    });
+
+    test('it should correctly convert bytes to megabytes', () => {
+        expect(bytesToSize(1048576)).toBe('1 MB');
+        expect(bytesToSize(2097152)).toBe('2 MB');
+        expect(bytesToSize(3145728)).toBe('3 MB');
+        expect(bytesToSize(1572864)).toBe('2 MB');
+    });
+
+    test('it should correctly convert bytes to gigabytes', () => {
+        expect(bytesToSize(1073741824)).toBe('1 GB');
+        expect(bytesToSize(2147483648)).toBe('2 GB');
+        expect(bytesToSize(3221225472)).toBe('3 GB');
+        expect(bytesToSize(1610612736)).toBe('2 GB');
+    });
+
+    test('it should correctly convert bytes to terabytes', () => {
+        expect(bytesToSize(1099511627776)).toBe('1 TB');
+        expect(bytesToSize(2199023255552)).toBe('2 TB');
+        expect(bytesToSize(3298534883328)).toBe('3 TB');
+        expect(bytesToSize(1649267441664)).toBe('2 TB');
+    });
+});
