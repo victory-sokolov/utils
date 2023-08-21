5ecb74a823abbe08814f0d6cb483dd7a96e9e25d
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-08-21 11:55:52 +0300

    feat: Add throttle and debounce functions

--- src/base.ts
+++ src/base.ts
@@ -15,7 +15,7 @@ export const perfStart = () => performance.now();
 
 /**
  * End time of function
- * @param startTime 
+ * @param startTime
  */
 export const perfStop = (startTime: number) => {
     const endTime = performance.now();
@@ -35,4 +35,67 @@ export const bytesToSize = (bytes: number): string => {
     const short = Math.round(size);
     const unit = exp === 0 ? '' : ' ' + SIZE_UNITS[exp - 1];
     return short.toString() + unit;
+};
+
+/**
+ * Debouncing used to improve the performance of frequently executed actions,
+ * by delaying them, grouping them, and only executing the last call.
+ * @param fn Function to debounce
+ * @param delay Function delay
+ * @returns new function
+ */
+export const debounce = <T extends unknown[]>(fn: (...args: T) => void, delay: number) => {
+    let timeoutID: number | undefined;
+    let lastArgs: T | undefined;
+
+    const run = () => {
+        if (lastArgs) {
+            fn(...lastArgs);
+            lastArgs = undefined;
+        }
+    };
+
+    const debounced = (...args: T) => {
+        clearTimeout(timeoutID);
+        lastArgs = args;
+        timeoutID = window.setTimeout(run, delay);
+    };
+
+    debounced.flush = () => {
+        clearTimeout(timeoutID);
+        run();
+    };
+
+    return debounced;
+};
+
+/**
+ * @param fn Function to debounce used to improve the performance of frequently executed actions.
+ * Guarantees the regular execution of an action.
+ * @param cooldown Timer arg
+ * @returns a new function, which when executed, stores the call arguments and starts the cooldown timer
+ */
+export const throttle = <Args extends unknown[]>(fn: (...args: Args) => void, cooldown: number) => {
+    let lastArgs: Args | undefined;
+
+    const run = () => {
+        if (lastArgs) {
+            fn(...lastArgs);
+            lastArgs = undefined;
+        }
+    };
+
+    const throttled = (...args: Args) => {
+        const isOnCooldown = !!lastArgs;
+
+        lastArgs = args;
+
+        if (isOnCooldown) {
+            return;
+        }
+
+        window.setTimeout(run, cooldown);
+    };
+
+    return throttled;
 };
