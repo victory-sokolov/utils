1504075da1faa0983d3a80d68c7ef76c600f3114
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-03-06 19:56:54 +0200

    feat: timeAgo - get string time ago seconds/minutes/hours/days from date object

--- src/date.ts
+++ src/date.ts
@@ -159,3 +159,41 @@ export const secondsInDays = (days: number): number => {
     const secondsInMonth = days * secondsInDay;
     return secondsInMonth;
 };
+
+/**
+ * Convert DateTime object to time ago string
+ * @param date
+ * @returns
+ */
+export const timeAgo = (date: Date): string => {
+    const seconds = Math.floor((new Date() - date) / 1000);
+
+    let interval = Math.floor(seconds / 31536000);
+    if (interval > 1) {
+        return `${interval} years ago`;
+    }
+
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return `${interval} months ago`;
+    }
+
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return `${interval} days ago`;
+    }
+
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return `${interval} hours ago`;
+    }
+
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return `${interval} minutes ago`;
+    }
+
+    if (seconds < 10) return 'just now';
+
+    return `${Math.floor(seconds)} seconds ago`;
+};
