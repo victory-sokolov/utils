0338904fda361e45aa5e455d14155d0a7573dab6
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-03-07 12:49:23 +0200

    feat: tap - Pass the value through the callback, and return the value

--- src/function.ts
+++ src/function.ts
@@ -54,3 +54,20 @@ export const applyPipe = <FirstFn extends AnyFunc, F extends AnyFunc[]>(
  * @returns True if function is asynchronous
  */
 export const isAsync = (fn: Function) => fn.constructor.name === 'AsyncFunction';
+
+/**
+ * Pass the value through the callback, and return the value
+ *
+ * @example
+ * ```
+ * function createUser(name: string): User {
+ *   return tap(new User, user => {
+ *     user.name = name
+ *   })
+ * }
+ * ```
+ */
+export function tap<T>(value: T, callback: (value: T) => void): T {
+    callback(value);
+    return value;
+}
