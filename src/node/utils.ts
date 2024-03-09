74136fc60553aa8f7f24aba17faff467eaffc7bc
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-03-09 21:23:07 +0200

    feat: getCMDArgs

---
+++ src/node/utils.ts
@@ -0,0 +1,10 @@
+
+
+/**
+ * Get CMD arguments
+ * Example: 
+ * node my-script.js --name=John --age=30
+ * getCmdArgs(); // ['--name=John', '--age=30']
+ * @returns CMD args
+ */
+export const getCmdArgs = () => process.argv.slice(2);
