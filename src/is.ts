2a34c0a4615e32526377d3e2c708f95154018447
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-04-02 23:14:00 +0300

    fix isObject

--- src/is.ts
+++ src/is.ts
@@ -6,7 +6,8 @@ export const isBoolean = (val: any): val is boolean => typeof val === 'boolean';
 export const isFunction = <T extends Function>(val: any): val is T => typeof val === 'function';
 export const isNumber = (val: any): val is number => typeof val === 'number';
 export const isString = (val: unknown): val is string => typeof val === 'string';
-export const isObject = (val: any): val is object => toString(val) === '[object Object]';
+export const isObject = (val: unknown): val is Record<string, unknown> =>
+    typeof val === 'object' && val !== null;
 export const isUndefined = (val: any): val is undefined => toString(val) === '[object Undefined]';
 export const isNull = (val: any): val is null => toString(val) === '[object Null]';
 export const isRegExp = (val: any): val is RegExp => toString(val) === '[object RegExp]';
