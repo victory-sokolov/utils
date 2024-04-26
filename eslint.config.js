38f4aa5b17cba17347f42157efbdc827d98bce94
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-04-26 14:12:01 +0300

    add docs to ignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,19 @@
 import antfu from '@antfu/eslint-config';
 
 export default antfu({
+    ignores: ['docs'],
     stylistic: {
         indent: 4,
         semi: true,
+        overrides: {
+            'antfu/top-level-function': 'off',
+        },
     },
     typescript: {
         overrides: {
             'ts/ban-ts-comment': 'off',
             'ts/prefer-ts-expect-error': 'off',
+            'antfu/top-level-function': 'off',
         },
     },
 });
