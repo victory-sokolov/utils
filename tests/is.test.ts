9b761a6523a9d17bb02f291a265c0660d2db73ef
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-02-27 14:07:29 +0200

    run linter

--- tests/is.test.ts
+++ tests/is.test.ts
@@ -2,9 +2,9 @@ import { describe, expect, it } from 'vitest';
 import { isDate } from '../src/is';
 
 describe('test is utils', () => {
-  it('is date', () => {
-    expect(isDate(new Date())).toBe(true);
-    expect(isDate(new Date("2025-02-27T11:50:00.000Z'"))).toBe(true);
-    expect(isDate("2025-02-27T11:50:00.000Z'")).toBe(false);
-  });
+    it('is date', () => {
+        expect(isDate(new Date())).toBe(true);
+        expect(isDate(new Date('2025-02-27T11:50:00.000Z\''))).toBe(true);
+        expect(isDate('2025-02-27T11:50:00.000Z\'')).toBe(false);
+    });
 });
