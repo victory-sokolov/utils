efc827c68361fe1cfbd325e3634e9d4f4a392897
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-08-03 22:12:45 +0300

    exclude certain files from coverage

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,15 @@ export default defineConfig({
         environment: 'jsdom',
         coverage: {
             reporter: ['text', 'html'],
-            exclude: ['node_modules/'],
+            exclude: [
+                'node_modules/',
+                'eslint.config.js',
+                'vite.config.ts',
+                'rollup.config.js',
+                'release.config.cjs',
+                './dist',
+                './docs'
+            ],
         },
         resolve: {
             conditions: ['source'],
