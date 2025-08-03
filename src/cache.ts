e282d1d93e267c81909b3639e9aff35e92c807f2
Viktor Sokolov <viktorsokolov.and@gmail.com>
2025-08-03 22:34:06 +0300

    feat: add types for cache.ts

--- src/cache.ts
+++ src/cache.ts
@@ -1,24 +1,35 @@
-export const cache = (() => {
-    const store = {};
+interface CacheStore<T = any> {
+    [key: string]: T | undefined;
+}
+
+interface CacheAPI<T = any> {
+    set(key: string, value: T): void;
+    has(key: string): boolean;
+    get(key: string): T | undefined;
+    remove(key: string): void;
+}
+
+export const cache = <T = any>(): CacheAPI<T> => {
+    const store: CacheStore<T> = {};
 
     return {
-        set(key, value) {
+        set(key: string, value: T): void {
             store[key] = value;
         },
 
-        has(key) {
+        has(key: string): boolean {
             return !!this.get(key);
         },
 
-        get(key) {
+        get(key: string): T | undefined {
             return store[key];
         },
 
-        remove(key) {
+        remove(key: string): void {
             if (this.has(key)) {
-                store[key] = null;
+                store[key] = undefined;
                 delete store[key];
             }
         },
     };
-})();
+};
