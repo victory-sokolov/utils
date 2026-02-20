e572ebfd3bc9f8d4c8c60984492b9f19ccefe36c
Viktor Sokolov <viktorsokolov.and@gmail.com>
2026-02-20 09:40:03 +0200

    add few fiels to ignore list

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import alias from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 import filesize from 'rollup-plugin-filesize';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
 
 const entries = ['src/index.ts'];
 
